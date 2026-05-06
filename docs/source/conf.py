21d929f1d5199e0b3232e1eff31b175b0b9dc906
Yu-Yuan (Stuart) Yang from connectin - AFLab <yu-yuan.yang@qmul.ac.uk>
2026-05-06 12:21:36 +0100

    update with new model

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, str(Path("../..").resolve()))
 project = "FragBEST-Myo"
 author = "Yu-Yuan (Stuart) Yang"
 copyright = f"2024-{datetime.now().year}, {author}"
-release = "0.1.0"
+release = "0.2.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
